52b89dc4ff04d34f798f17a045bd6cade7e5c285
Lauren Voswinkel <lvoswinkel@hashicorp.com>
2020-10-22 15:23:21 -0700

    Removing tls argument from Serve call (#13)
    * Removing tls argument from Run call
    * Move Run into main package

--- cmd/couchbase-database-plugin/main.go
+++ cmd/couchbase-database-plugin/main.go
@@ -4,21 +4,28 @@ import (
 	"os"
 
 	hclog "github.com/hashicorp/go-hclog"
-
 	couchbase "github.com/hashicorp/vault-plugin-database-couchbase"
-	"github.com/hashicorp/vault/api"
+	dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
 )
 
 func main() {
-	apiClientMeta := &api.PluginAPIClientMeta{}
-	flags := apiClientMeta.FlagSet()
-	flags.Parse(os.Args[1:])
-
-	err := couchbase.Run(apiClientMeta.GetTLSConfig())
+	err := Run()
 	if err != nil {
 		logger := hclog.New(&hclog.LoggerOptions{})
 
 		logger.Error("plugin shutting down", "error", err)
 		os.Exit(1)
 	}
+}
+
+// Run instantiates a CouchbaseDB object, and runs the RPC server for the plugin
+func Run() error {
+	db, err := couchbase.New()
+	if err != nil {
+		return err
+	}
+
+	dbplugin.Serve(db.(dbplugin.Database))
+
+	return nil
 }
