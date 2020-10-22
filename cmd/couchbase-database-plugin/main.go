fccad073dea597a2f1a144f03c955f1615651a24
Lauren Voswinkel <lvoswinkel@hashicorp.com>
2020-10-22 14:42:49 -0700

    Move Run into main package

--- cmd/couchbase-database-plugin/main.go
+++ cmd/couchbase-database-plugin/main.go
@@ -5,6 +5,7 @@ import (
 
 	hclog "github.com/hashicorp/go-hclog"
 
+	dbplugin "github.com/hashicorp/vault/sdk/database/dbplugin/v5"
 	couchbase "github.com/hashicorp/vault-plugin-database-couchbase"
 	"github.com/hashicorp/vault/api"
 )
@@ -14,11 +15,23 @@ func main() {
 	flags := apiClientMeta.FlagSet()
 	flags.Parse(os.Args[1:])
 
-	err := couchbase.Run(apiClientMeta.GetTLSConfig())
+	err := Run()
 	if err != nil {
 		logger := hclog.New(&hclog.LoggerOptions{})
 
 		logger.Error("plugin shutting down", "error", err)
 		os.Exit(1)
 	}
+}
+
+// Run instantiates a CouchbaseDB object, and runs the RPC server for the plugin
+func Run() error {
+	db, err := couchbase.New()
+	if err != nil {
+		return err
+	}
+
+	dbplugin.Serve(db.(dbplugin.Database))
+
+	return nil
 }
